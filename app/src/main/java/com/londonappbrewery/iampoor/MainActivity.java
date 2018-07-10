d898625a134011610855c58ca5d57f8e2c08dfce
Ryynanen, Petri <petri.ryynanen@accenture.comgit clone https://petri.ryynanen@innersource.accenture.com/scm/afhrtvkn/hrrt-test-scripts.gitgit config --global user.email petri.ryynanen@accenture.com>
2018-07-11 01:59:51 +0300

    Added sound to image

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -3,16 +3,19 @@ package com.londonappbrewery.iampoor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mMediaPlayer;
+    MediaPlayer sadSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sadSound = MediaPlayer.create(getApplicationContext(), R.raw.sad);
         mMediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.fall);
         mMediaPlayer.start();
     }
@@ -27,5 +30,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
         mMediaPlayer.start();
+    }
+
+    public void playSadSound(View v){
+        sadSound.start();
     }
 }
